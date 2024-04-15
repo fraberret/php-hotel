27256f1a6354597d6885ab0ff74e223a5d43e0ad
fraberret <f.berretta02@gmail.com>
2024-04-15 15:15:18 +0200

    add name in page by cicle

--- index.php
+++ index.php
@@ -40,4 +40,33 @@
 
     ];
 
+
+    
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php foreach($hotels as $hotel) :?>
+
+<h1> 
+    <?= $hotel['name']?>
+
+</h1>
+
+<?php endforeach;?>
+
+
+
+
+
+</body>
+</html>
